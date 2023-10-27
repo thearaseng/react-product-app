918652424fd30da8018e55061bb6179542693d81
Theara Seng <me@thearaseng.com>
2023-10-27 15:29:48 -0500

    allow cors from *

--- backend/src/main/java/com/example/lab12backend/config/WebConfig.java
+++ backend/src/main/java/com/example/lab12backend/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("*")
                         .allowedHeaders("*")
                         .allowedMethods("*");
             }
